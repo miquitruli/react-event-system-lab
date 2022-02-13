d77272a54a4ed23f7d1aad87389d987107ce12e6
Micaella Colombo <micaelc@g.clemson.edu>
2022-02-13 13:39:53 -0500

    commit tree 4d7005bd7a342fdaf0df3a4ffbef6bbe0eeb7443

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,15 @@
-// Code Keypad Component Here
+
+import React from 'react';
+
+export default class Keypad extends React.Component {
+    passwordAction = () => {
+        console.log("Entering password...");
+    };
+    
+    
+    render() {
+        return (
+            <input onKeyUp={this.passwordAction} type="password" />
+        )
+    };
+}
